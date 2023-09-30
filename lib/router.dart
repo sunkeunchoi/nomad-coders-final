95a3b87ed3a697df0b98f8bf9255d3dfdc415e02
sunkeunchoi <sunkeun.choi@gmail.com>
2023-09-30 12:40:39 +0900

    router

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:proxima_nomadcoders/pages/home_page.dart';
 
 // final router = Provider((ref) {
 //   // final user = ref.watch(authState);
