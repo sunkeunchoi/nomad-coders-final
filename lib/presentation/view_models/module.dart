4c6776001384d9c0073050a25228f7913a30b509
sunkeunchoi <sunkeun.choi@gmail.com>
2023-09-30 20:28:37 +0900

    add profile view_model

--- lib/presentation/view_models/module.dart
+++ lib/presentation/view_models/module.dart
@@ -1,32 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../domain/moods/entities/mood.dart';
-import '../../domain/moods/usecases/module.dart';
-
-class MoodsStateNotifier extends StateNotifier<List<Mood>> {
-  MoodsStateNotifier(this.ref) : super([]) {
-    loadMoods();
-  }
-  final Ref ref;
-  late final getMoods = ref.read(getMoodsUseCase);
-  Future<void> loadMoods() async {
-    state = await getMoods();
-  }
-
-  Future<void> add(Mood mood) async {
-    await ref.read(saveMoodUseCase).call(mood);
-    await loadMoods();
-  }
-
-  Future<void> delete(Mood mood) async {
-    await ref.read(deleteMoodUseCase).call(mood);
-    await loadMoods();
-  }
-
-  Future<Mood?> get(String id) async {
-    return await ref.read(getMoodUseCase).call(id);
-  }
-}
+import '../../domain/profiles/entities/profile.dart';
+import 'moods_view_model.dart';
+import 'profiles_view_model.dart';
 
 final moodListState = StateNotifierProvider<MoodsStateNotifier, List<Mood>>(
   (ref) => MoodsStateNotifier(ref),
@@ -34,4 +11,12 @@ final moodListState = StateNotifierProvider<MoodsStateNotifier, List<Mood>>(
 
 final moodListModel = Provider<MoodsStateNotifier>(
   (ref) => ref.watch(moodListState.notifier),
+);
+
+final profileState = StateNotifierProvider<ProfileStateNotifier, Profile?>(
+  (ref) => ProfileStateNotifier(ref),
+);
+
+final profileModel = Provider<ProfileStateNotifier>(
+  (ref) => ref.watch(profileState.notifier),
 );
---
+++ lib/presentation/view_models/profiles_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../domain/profiles/entities/profile.dart';
+import '../../domain/profiles/usecases/module.dart';
+
+class ProfileStateNotifier extends StateNotifier<Profile?> {
+  ProfileStateNotifier(this.ref) : super(null) {
+    loadProfile();
+  }
+  final Ref ref;
+  late final getProfile = ref.read(getCurrentUserUseCase);
+  Future<void> loadProfile() async {
+    state = await getProfile();
+  }
+
+  Future<void> signIn({required String email, required String password}) async {
+    await ref.read(signInUseCase).call(email, password);
+    await loadProfile();
+  }
+
+  Future<void> signOut() async {
+    await ref.read(signOutUseCase).call();
+  }
+
+  Future<void> signUp({required String email, required String password}) async {
+    await ref.read(signUpUseCase).call(email, password);
+  }
+}
