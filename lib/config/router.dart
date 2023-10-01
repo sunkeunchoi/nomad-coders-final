4e798e9cc49b072e4a8c73debc8ac5183ecc1ad5
sunkeunchoi <sunkeun.choi@gmail.com>
2023-10-01 13:16:16 +0900

    fix router

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:proxima_nomadcoders/domain/moods/entities/mood.dart';
 import 'package:proxima_nomadcoders/presentation/moods_page.dart';
 import 'package:proxima_nomadcoders/presentation/sign_in_page.dart';
 
